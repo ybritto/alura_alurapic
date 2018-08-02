36c7e411537ce6adcfe8c336f7f84399b89dca48
Britto <ybritto@deloitte.pt>
2018-08-02 17:39:28 +0100

    First commit

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,12 @@
+angular.module('alurapic').controller('FotosController', function($scope, $http){
+
+	$scope.fotos = [];
+
+	$http.get('http://localhost:3000/v1/fotos').success(function(retorno){
+		console.log(retorno);
+		$scope.fotos = retorno;
+	}).error(function(error){
+		console.log(error);
+	});
+
+});
