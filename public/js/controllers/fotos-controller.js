c5505cb68f97bfb91148913a9a3ff95c03d7dfb1
Britto <ybritto@deloitte.pt>
2018-08-07 15:02:59 +0100

    Filtering images with ngAnimation

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,6 +1,7 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http){
 
 	$scope.fotos = [];
+	$scope.filtro = '';
 
 	$http.get('http://localhost:3000/v1/fotos').success(function(retorno){
 		console.log(retorno);
