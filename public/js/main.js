7c911029742715ae93113b2a2fad7e31398ae2e9
Britto <ybritto@deloitte.pt>
2018-08-08 15:14:42 +0100

    Adding formulario to add new picturies

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,18 @@
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate']);
+angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+	.config(function($routeProvider, $locationProvider){
+
+		$locationProvider.html5Mode(true);
+		$routeProvider.when('/fotos', {
+			templateUrl: 'partials/principal.html',
+			controller: 'FotosController'
+		})
+
+		$locationProvider.html5Mode(true);
+		$routeProvider.when('/fotos/new', {
+			templateUrl: 'partials/foto.html',
+			controller: 'FotoController'
+		})
+
+		$routeProvider.otherwise({redirectTo: '/fotos'});
+
+	});
